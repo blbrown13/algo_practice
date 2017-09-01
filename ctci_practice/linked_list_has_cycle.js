d68b5c9a1f55340e1b1eef3524b2652fdb15e8e4
Brandon Brown <blbrown13@gmail.com>
2017-09-01 13:29:23 -0700

    Refactor code including ES6 style

--- ctci_practice/linked_list_has_cycle.js
+++ ctci_practice/linked_list_has_cycle.js
@@ -1,48 +1,52 @@
+
 // ** Linked Lists: Detect a Cycle **
 
-// create node class
-var Node = function(data, next) {
+'use strict';
+
+// create node class using ES6 style
+class Node {
+  constructor(data, next) {
     this.data = data;
     this.next = next;
+  }
 }
 
 // function to detect a cycle in a linked list
-var hasCycle = function(head) {
-    var history = [];
-    var hasCycle = false;
+const hasCycle = (head) => {
+  var history = [];
+  var hasCycle = false;
 
-	var iterate = function(node) {
-		if (history.includes(node)) {
-			hasCycle = true;
-        } else {
-			if (node.next) {
-				history.push(node);
-				iterate(node.next);
-            }
-    	}
+  var iterate = function(node) {
+    if (history.includes(node)) {
+      hasCycle = true;
+    } else {
+      if (node.next) {
+        history.push(node);
+        iterate(node.next);
+      }
     }
+  }
 
-	iterate(head);
-	return hasCycle;
+  iterate(head);
+  return hasCycle;
 }
 
-var a = new Node('a');
-var b = new Node('b');
-var c = new Node('c');
-
+// create new instances of nodes
+let a = new Node('a');
+let b = new Node('b');
+let c = new Node('c');
+// link nodes with cycle
 a.next = b;
 b.next = c;
 c.next = b;
 
-console.log(a);
-console.log(b);
-console.log(c);
+let resultA = hasCycle(a);
+console.log(resultA); // expect true
 
-console.log(hasCycle(a));
-
-
-var d = new Node('d');
-var e = new Node('e');
+// create new nodes with no cycle
+let d = new Node('d');
+let e = new Node('e');
 d.next = e;
 
-console.log(hasCycle(d));
+let resultD = hasCycle(d);
+console.log(resultD); // expect false
